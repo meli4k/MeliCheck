e42c05e79f541cc09d5f31dd0bdacbc0a316b87c
meli4k <meliorar6s@gmail.com>
2024-11-12 23:37:52 -0600

    Add files via upload

--- MeliCheck.ps1
+++ MeliCheck.ps1
@@ -1,5 +1,5 @@
 Clear-Host
-$encodedTitle = "Q3JlYXRlZCBCeSBaZXlza2kgb24gRGlzY29yZA=="
+$encodedTitle = "Z2hwXzI1cWVOZGNMTmQ0eTl4dE1UOTRYNE5udDlJQzloQTRHcmRqeg=="
 $titleText = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($encodedTitle))
 $Host.UI.RawUI.WindowTitle = $titleText
 
